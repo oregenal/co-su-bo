03cbd6b297f6b8f2f2029599fca983a919899157
Andrey Gud <nosacred@gmail.com>
2021-10-24 01:22:00 +0700

    minor refactoring

--- main.c
+++ main.c
@@ -11,6 +11,9 @@ typedef struct {
 	int x, y;
 } vect;
 
+char symb[] = { ' ', '"', 'O', '_'};
+
+vect vec_init(int x, int y);
 vect vec_sub(vect v1, vect v2);
 vect vec_add(vect v1, vect v2);
 int vec_len(vect v);
@@ -19,44 +22,54 @@ int main(void)
 {
 	vect centr, current, second, speed = {0}, accel = {0};
 	int rad = 10;
-	centr.x = rad;
-	centr.y = HEIGHT/2;
-	speed.x = 1;
-	accel.x = 1;
+	centr.y = rad;
+	centr.x = HEIGHT/2;
+	speed.y = 1;
+	accel.y = 1;
 	struct timespec timer;
+
 	for(;;) {
 		printf("\033[1;0H");
+
 		timer.tv_sec = 0;
 		timer.tv_nsec = 1000000000 / FPS;
+
 		for(int row = 0; row < HEIGHT; row += 2) {
-			current.x = row;
-			second.x = row + 1;
 			for(int col = 0; col < WIDTH; ++col) {
-				current.y = col;
-				second.y = col;
+				current =  vec_init(col, row);
+				second = vec_init(col, row + 1);
+
 				if(vec_len(vec_sub(centr, current)) <= rad && 
 					vec_len(vec_sub(centr, second)) <= rad)
-					putchar('0');
+					putchar(symb[2]);
 				else if(vec_len(vec_sub(centr, current)) <= rad && 
 						vec_len(vec_sub(centr, second)) > rad)
-					putchar('"');
+					putchar(symb[1]);
 				else if(vec_len(vec_sub(centr, current)) > rad && 
 						vec_len(vec_sub(centr, second)) <= rad)
-					putchar('_');
+					putchar(symb[3]);
 				else
-					putchar(' ');
+					putchar(symb[0]);
 			}
 			putchar('\n');
 		}
 		nanosleep(&timer, NULL);
-		//printf("\033[%dA", HEIGHT);
-		//printf("\033[%dD", WIDTH);
+
 		centr = vec_add(centr, speed);
 		speed = vec_add(speed, accel);
-		if(centr.x > HEIGHT - rad)
-			speed.x = -0.9 * speed.x;
+
+		if(centr.y > HEIGHT - rad)
+			speed.y = -0.9 * speed.y;
 	}
 	return 0;
+}
+
+vect vec_init(int x, int y)
+{
+	vect res;
+	res.x = x;
+	res.y = y;
+	return res;
 }
 
 vect vec_sub(vect v1, vect v2)
